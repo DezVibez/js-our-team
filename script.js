35595fa311d2a84b521db24036e70112eb8a2898
Emanuele <103491766+DezVibez@users.noreply.github.com>
2022-07-05 15:46:01 +0200

    push * ticket

--- script.js
+++ script.js
@@ -22,11 +22,30 @@ Angela Lopez  |	Social Media Manager |	angela-lopez-social-media-manager.jpg
 Scott Estrada |	Developer            |	scott-estrada-developer.jpg
 Barbara Ramos |	Graphic Designer     |	barbara-ramos-graphic-designer.jpg */ 
 
-const dati = [{name: "Wayne Barnett", role: "Founder & CEO", photo: "wayne-barnett-founder-ceo.jpg"}, 
+
+const targetPrint = document.getElementById("targetPrint")
+
+const data = [{name: "Wayne Barnett", role: "Founder & CEO", photo: "wayne-barnett-founder-ceo.jpg"}, 
             {name: "Angela Caroll", role: "Chief Editor", photo: "angela-caroll-chief-editor.jpg"},
             {name: "Walter Gordon ", role: "Office Manager", photo: "walter-gordon-office-manager.jpg"},
             {name: "Angela Lopez", role: "Social Media Manager", photo: "angela-lopez-social-media-manager.jpg"},
             {name: "Scott Estrada", role: "Developer", photo: "scott-estrada-developer.jpg"},
             {name: "Barbara Ramos", role: "Graphic Designer", photo: "barbara-ramos-graphic-designer.jpg"}]
 
-console.log(dati) //===> MILESTONE1
+console.log(data) //===> MILESTONE1
+
+targetPrint.innerText = data[0,1,2,3,4,5]
+
+let arrayElement = ""
+
+for (let i = 0; i < data.length; i++) {
+	
+    arrayElement += data[i]
+}
+
+targetPrint.innerText = arrayElement
+
+
+
+
+
